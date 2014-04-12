98d5d0d1e899595225ddbca23a72306bd0a8d602
Robert Fotino <robert.fotino@gmail.com>
2014-04-12 15:25:20 -0700

    Added auth_ prefix to POST credentials.

--- resources/global.php
+++ resources/global.php
@@ -11,20 +11,20 @@ if (!$db) {
     $ERRORS++;
 }
 
-if (isset($_POST['username'])) {
-    $USERNAME = $_POST['username'];
+if (isset($_POST['auth_username'])) {
+    $AUTH_USERNAME = $_POST['auth_username'];
 }
-if (isset($_POST['password'])) {
-    $PASSWORD = $_POST['password'];
+if (isset($_POST['auth_password'])) {
+    $AUTH_PASSWORD = $_POST['auth_password'];
 }
 
 if (defined("REQUIRES_AUTHENTICATION") && REQUIRES_AUTHENTICATION) {
-    if (!isset($USERNAME) || !isset($PASSWORD)) {
+    if (!isset($AUTH_USERNAME) || !isset($AUTH_PASSWORD)) {
         echo json_encode(array("error" => "Authentication required."));
         exit;
-    } else if (!valid_login($USERNAME, $PASSWORD)) {
+    } else if (!valid_login($AUTH_USERNAME, $AUTH_PASSWORD)) {
         echo json_encode(array("error" => "Invalid credentials."));
         exit;
     }
-    $CURRENT_USER = get_user_from_username($USERNAME);
+    $CURRENT_USER = get_user_from_username($AUTH_USERNAME);
 }
