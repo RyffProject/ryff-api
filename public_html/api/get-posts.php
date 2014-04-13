8b3d9d2ad8f9f3aac80fd03b774f8f5e6678b03e
Robert Fotino <robert.fotino@gmail.com>
2014-04-13 03:05:58 -0700

    Added the ability to get posts from a single user.

---
+++ public_html/api/get-posts.php
@@ -0,0 +1,50 @@
+<?php
+
+define("REQUIRES_AUTHENTICATION", true);
+
+set_include_path(implode(PATH_SEPARATOR, array(
+    get_include_path(),
+    __DIR__."/../../resources"
+)));
+
+require_once("global.php");
+
+if (isset($_POST['id'])) {
+    $USER_ID = (int)$_POST['id'];
+} else {
+    $USER_ID = $CURRENT_USER->id;
+}
+
+if (isset($_POST['exclude'])) {
+    $exclude_ids = explode(",", $_POST['exclude']);
+    foreach ($exclude_ids as &$id) {
+        $id = $db->real_escape_string((int)$id);
+    }
+} else {
+    $exclude_ids = array(0);
+}
+
+$num_posts = isset($_POST['limit']) ? (int)$_POST['limit'] : 5;
+
+$query = "SELECT * FROM `posts`
+          WHERE `user_id`=".$db->real_escape_string($USER_ID)."
+          AND `post_id` NOT IN (".implode(",", $exclude_ids).")
+          ORDER BY `date_created` DESC
+          LIMIT ".$db->real_escape_string($num_posts);
+$results = $db->query($query);
+
+if ($results) {
+    $posts = array();
+    while ($row = $results->fetch_assoc()) {
+        $post = get_post_from_id($row['post_id']);
+        if ($post) {
+            $posts[] = $post;
+        }
+    }
+    echo json_encode(array(
+        "success" => "Retrieved posts successfully.",
+        "posts" => $posts
+        ));
+} else {
+    echo json_encode(array("error" => "There was an error getting the user's posts."));
+}
