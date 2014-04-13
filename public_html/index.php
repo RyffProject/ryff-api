190496c4472b57bfc295f765b9ec559150dfc26f
Robert Fotino <robert.fotino@gmail.com>
2014-04-13 02:53:27 -0700

    Added an index for the root of the site.

---
+++ public_html/index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>ryff.me</title>
+        <style type="text/css">
+            h1 {
+                font-size: 10em;
+                font-family: sans-serif;
+                text-align: center;
+                padding: 50px 0 0 0;
+                margin: 0;
+            }
+            h2 {
+                font-size: 1.5em;
+                font-family: sans-serif;
+                text-align: center;
+                padding: 30px 0 0 0;
+                margin: 0;
+                color: #999;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <h1>ryff.me</h1>
+        <h2>coming soon to an app store near you</h2>
+    </body>
+</html>
