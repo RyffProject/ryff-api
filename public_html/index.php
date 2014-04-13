ce8c9ed9d349f91cf7c59ec098ab3fbbea3a2979
Robert Fotino <robert.fotino@gmail.com>
2014-04-13 10:59:32 -0700

    Added a single page site

--- public_html/index.php
+++ public_html/index.php
@@ -1,27 +1,50 @@
 <html>
     <head>
-        <title>ryff.me</title>
-        <style type="text/css">
-            h1 {
-                font-size: 10em;
-                font-family: sans-serif;
-                text-align: center;
-                padding: 50px 0 0 0;
-                margin: 0;
-            }
-            h2 {
-                font-size: 1.5em;
-                font-family: sans-serif;
-                text-align: center;
-                padding: 30px 0 0 0;
-                margin: 0;
-                color: #999;
-            }
-        </style>
+        <title>Ryff | Never Jam Alone</title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
     </head>
     
     <body>
-        <h1>ryff.me</h1>
-        <h2>coming soon to an app store near you</h2>
+        <div id="header">
+            <h1>Ryff</h1>
+            <div class="align-right"><h2>Never Jam Alone</h2></div>
+        </div>
+        
+        <div id="content">
+            <img src="images/ryff-screenshot.png" width="325px" height="525px" class="screenshot" />
+            
+            <p>
+                Ryff was conceived at LA Hacks 2014 by Chris Laganiere and Robert 
+                Fotino, two first-year computer science students at UCLA.
+            </p>
+            
+            <p>
+                Ryff is an app to help musicians find each other. You can create an artist 
+                profile, add your musical interests, and connect with other local 
+                musicians looking to play. Ryff allows you to record and post 
+                samples of your music without leaving the app, so you can show off your talents
+                to people in the area. Do your friends have a band, but nobody 
+                wants to play drums? Start a group on Ryff and search for nearby 
+                artists who play drums. Trying to find a guitarist who's into a 
+                specific genre? You can do that, too.
+            </p>
+
+            <p>
+                Ryff allows you to connect with the people that you want to be 
+                connected with. You can browse nearby artists and musical groups 
+                with similar tastes, hear how they sound, and either add them to 
+                your favorites or skip them and move on to the next. If you like 
+                the music of a particular artist, you should message them and meet up. 
+                Here at Ryff it's our belief that you should <em>Never Jam Alone</em>.
+            </p>
+            
+            <p>
+                If you have an iPhone, Ryff is coming soon to an app store near you. 
+                Android and web interfaces are in the works. If you have any 
+                questions or feedback, you can email the creators at
+                <a href="mailto:robert.fotino@gmail.com">robert.fotino@gmail.com</a> and 
+                <a href="mailto:chrislaganiere@gmail.com">chrislaganiere@gmail.com</a>.
+            </p>
+        </div>
     </body>
 </html>
