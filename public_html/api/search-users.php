7011ac7e47bd054ac54b511b12e26df0466e2862
Robert Fotino <robert.fotino@gmail.com>
2014-04-13 06:34:09 -0700

    Added a search-users function and fixed get users nearby.
    The search users function searches by name, username, bio, post content, genres, instruments, and riff titles, and sorts the users by geographic proximity.

--- public_html/api/search-users.php
+++ public_html/api/search-users.php
@@ -26,42 +26,58 @@ if (isset($_POST['exclude'])) {
 
 $num_users = isset($_POST['limit']) ? (int)$_POST['limit'] : 5;
 
-//Select the closest users not in the $_POST['ids'] array, which are excluded
-//because they have already been sent to the client
-$query = "SELECT u.`user_id`, u.`name`, u.`username`, u.`email`, u.`bio`, u.`date_updated`,
+$query_str = isset($_POST['query']) ? trim($_POST['query']) : "";
+$safe_query_str = $db->real_escape_string($query_str);
+
+$query = "SELECT DISTINCT(u.`user_id`), u.`name`, u.`username`, 
+          u.`email`, u.`bio`, u.`date_created`,
           SQRT(POW(X(l.`location`)-".$db->real_escape_string($user_location->x).",2)+
           POW(Y(l.`location`)-".$db->real_escape_string($user_location->y).",2)) AS `distance`
           FROM `users` AS u
+          LEFT JOIN `posts` AS p
+          ON p.`user_id`=u.`user_id`
+          LEFT JOIN `genres` AS g
+          ON g.`user_id`=u.`user_id`
+          LEFT JOIN `instruments` AS i
+          ON i.`user_id`=u.`user_id`
+          LEFT JOIN `riffs` AS r
+          ON r.`post_id`=p.`post_id`
           LEFT JOIN `locations` AS l
-          ON l.`user_id` = u.`user_id` AND u.`active`=1
-          WHERE l.`user_id` NOT IN (".implode(",", $exclude_ids).")
+          ON l.`user_id`=u.`user_id`
+          WHERE u.`active`=1
+          AND u.`user_id` NOT IN (".implode(",", $exclude_ids).")
           AND l.`date_created`=(
               SELECT MAX(l2.`date_created`) 
               FROM `locations` AS l2 
               WHERE l2.`user_id`= l.`user_id`
           )
-          AND l.`user_id`!=".$db->real_escape_string($CURRENT_USER->id)."
+          AND 
+          (
+              u.`name` LIKE '%$safe_query_str%'
+              OR u.`username` LIKE '%$safe_query_str%'
+              OR u.`bio` LIKE '%$safe_query_str%'
+              OR g.`genre` LIKE '%$safe_query_str%'
+              OR i.`instrument` LIKE '%$safe_query_str%'
+              OR p.`content` LIKE '%$safe_query_str%'
+              OR r.`title` LIKE '%$safe_query_str%'
+          )
           ORDER BY `distance` ASC
           LIMIT ".$db->real_escape_string($num_users);
 $results = $db->query($query);
-
-if ($results && $results->num_rows) {
+if ($results) {
     $users = array();
     while ($row = $results->fetch_assoc()) {
-        $user = new User($row['user_id'], $row['name'], $row['username'], 
+        $user = new User($row['user_id'], $row['name'], $row['username'],
                 $row['email'], $row['bio'], $row['date_created']);
         if ($user) {
             $users[] = $user;
         }
     }
-    if (count($users)) {
-        echo json_encode(array(
-            "success" => "Found some users nearby.",
-            "users" => $users
-            ));
-    } else {
-        echo json_encode(array("error" => "Could not find any users."));
-    }
-} else {
-    echo json_encode(array("error" => "There was an error processing your request."));
+    echo json_encode(array(
+        "success" => "Retrieved users successfully.",
+        "users" => $users
+        ));
+    exit;
 }
+
+echo json_encode(array("error" => "Unable to retrieve users."));
