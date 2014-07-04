924c54ac925455fa2890c199a1871be4baf658f3
Robert Fotino <robert.fotino@gmail.com>
2014-07-03 19:36:05 -0700

    Refs #2, added a "karma" field to the user class.
    The user's karma is the total number of upvotes people have given to his
    or her posts.

--- resources/user.class.php
+++ resources/user.class.php
@@ -8,8 +8,11 @@ class User {
     public $bio;
     public $date_created;
     public $avatar;
+    public $karma;
     
     function __construct($id, $name, $username, $email, $bio, $date_created) {
+        global $db;
+        
         $this->id = (int)$id;
         $this->name = $name;
         $this->username = $username;
@@ -17,5 +20,25 @@ class User {
         $this->bio = $bio;
         $this->date_created = $date_created;
         $this->avatar = get_avatar_url($id);
+        $this->karma = User::getKarma($this->id);
+    }
+    
+    static function getKarma($user_id) {
+        global $db;
+        
+        $karma_query = "SELECT SUM(c.`num_upvotes`) AS `karma`
+                        FROM (
+                          SELECT COUNT(*) AS `num_upvotes`
+                          FROM `upvotes` AS a
+                          JOIN `posts` AS b
+                          ON b.`post_id`=a.`post_id`
+                          WHERE b.`user_id`=".$db->real_escape_string((int)$user_id)."
+                        ) AS c";
+        $karma_results = $db->query($karma_query);
+        if ($karma_results && $karma_results->num_rows) {
+            $karma_row = $karma_results->fetch_assoc();
+            return (int)$karma_row['karma'];
+        }
+        return 0;
     }
 }
