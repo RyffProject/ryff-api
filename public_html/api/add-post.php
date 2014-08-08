f1dd2d80a3aca681a00754bf734c5efb369b977a
Chris Laganiere <ChrisLaganiere@ucla.edu>
2014-08-08 15:54:05 -0700

    Update add-post to reflect parent_ids
    Clarification that posts can have multiple parents

--- public_html/api/add-post.php
+++ public_html/api/add-post.php
@@ -12,7 +12,7 @@
  * POST variables:
  * "title" The title of the post. Titles longer than 255 characters will be truncated.
  * "content" The body of the post. Bodies longer than 65535 bytes will be truncated.
- * "parent_id" (optional) The id of the parent post, if this is a reply.
+ * "parent_ids" (optional) Array of ids of the parent posts sampled in this post's riff.
  * 
  * File uploads:
  * "riff" An .m4a audio file.
