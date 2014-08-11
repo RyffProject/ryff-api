bfbebf0f81cfc96d6f305af22ca8b50c5a376e08
Robert Fotino <robert.fotino@gmail.com>
2014-08-11 14:03:03 -0700

    Refs #28, adds get-tags-suggested script.
    This fetches up to 10 tags that it thinks the current user would like to search for. Right now it does this by getting the most popular tags on the users that the current user follows or their posts.

---
+++ public_html/api/get-tags-suggested.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Get Tags Suggested
+ * ==================
+ * 
+ * Authentication required.
+ * Returns an array of no more than 10 tags to suggest the user search for.
+ * 
+ * Return on success:
+ * "success" The success message.
+ * "tags" An array of the suggested tags.
+ * 
+ * Return on error:
+ * "error" The error message.
+ * 
+ * Ryff API <http://www.github.com/rfotino/ryff-api>
+ * Released under the Apache License 2.0.
+ */
+
+define("REQUIRES_AUTHENTICATION", true);
+
+set_include_path(implode(PATH_SEPARATOR, array(
+    get_include_path(),
+    __DIR__."/../../resources"
+)));
+
+require_once("global.php");
+
+//Selects popular tags belonging to users the current user follows and their posts
+$query = "SELECT t.`tag`, COUNT(t.`tag`) AS `score`
+          FROM (
+            SELECT pt.`tag` AS `tag`
+            FROM `post_tags` AS pt
+            JOIN `posts` AS p
+            ON p.`post_id` = t.`post_id`
+            JOIN `users` AS u
+            ON u.`user_id` = p.`user_id`
+            JOIN `follows` AS f
+            ON f.`from_id` = p.`from_id`
+            WHERE f.`from_id` = ".$db->real_escape_string($CURRENT_USER->id)."
+          
+            UNION ALL
+
+            SELECT ut.`tag` AS `tag`
+            FROM `user_tags` AS ut
+            JOIN `users` AS u
+            ON u.`user_id` = t.`user_id`
+            JOIN `follows` AS f
+            ON f.`from_id` = u.`user_id`
+            WHERE f.`from_id` = ".$db->real_escape_string($CURRENT_USER->id)."
+          ) AS t
+          GROUP BY t.`tag`
+          ORDER BY `score` DESC
+          LIMIT 10";
+$results = $db->query($query);
+if ($results) {
+    $tags = array();
+    while ($row = $results->fetch_assoc()) {
+        $tags[] = $row['tag'];
+    }
+    echo json_encode(array(
+        "success" => "Successfully retrieved suggested tags.",
+        "tags" => $tags
+    ));
+} else {
+    echo json_encode(array("error" => "Error retrieving suggested tags."));
+}
