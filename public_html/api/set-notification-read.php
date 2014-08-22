7cdd14290b7c69b3d165b0339b58a098e477ea24
Robert Fotino <robert.fotino@gmail.com>
2014-08-22 00:31:50 -0700

    Refs #41, adds get-notifications and set-notifications-read.
    Also makes some changes/fixes to the Notification model.

---
+++ public_html/api/set-notification-read.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Set Notification Read
+ * =====================
+ * 
+ * Authentication required.
+ * Sets the given notification as read, if it belongs to the current user.
+ * 
+ * POST variables:
+ * "id" (required) The id of the notification you want to mark as read.
+ * 
+ * Return on success:
+ * "success" The success message.
+ * "notification" The updated notification object.
+ * 
+ * Return on error:
+ * "error" The error message.
+ * 
+ * Ryff API <http://www.github.com/rfotino/ryff-api>
+ * Released under the Apache License 2.0.
+ */
+
+define("REQUIRES_AUTHENTICATION", true);
+
+set_include_path(implode(PATH_SEPARATOR, array(
+    get_include_path(),
+    __DIR__."/../../resources"
+)));
+
+require_once("global.php");
+
+$notification_id = isset($_POST['id']) ? (int)$_POST['id'] : 0;
+$notification = Notification::get_by_id($notification_id);
+if (!$notification) {
+    echo json_encode(array(
+        "error" => "This notification either does not exist, ".
+                   "or you do not have permission to mark it as read."
+    ));
+    exit;
+}
+
+if ($notification->is_read) {
+    echo json_encode(array(
+        "error" => "This notification has already been marked as read."
+    ));
+    exit;
+}
+
+if (Notification::set_read($notification->id)) {
+    echo json_encode(array(
+        "success" => "Marked notification as read.",
+        "notification" => Notification::get_by_id($notification->id)
+    ));
+} else {
+    echo json_encode(array(
+        "error" => "There was an error marking this notification as read."
+    ));
+}
--- resources/models/notification.class.php
+++ resources/models/notification.class.php
@@ -19,17 +19,57 @@ class Notification {
         $this->$type = $object;
     }
     
-    public static function set_read($notification_id, $is_read) {
+    protected static function create($row) {
+        $object = null;
+        $type = "object";
+        if ($row['message_obj_id']) {
+            $object = Message::get_by_id((int)$row['message_obj_id']);
+            $type = "message";
+        } else if ($row['post_obj_id']) {
+            $object = Post::get_by_id((int)$row['post_obj_id']);
+            $type = "post";
+        } else if ($row['user_obj_id']) {
+            $object = User::get_by_id((int)$row['user_obj_id']);
+            $type = "user";
+        }
+        return new Notification(
+            $row['notification_id'], $row['text'], $row['read'],
+            $row['date_read'], $row['date_created'], $object, $type
+        );
+    }
+    
+    public static function set_read($notification_id) {
         global $db;
         $query = "
             UPDATE `notifications`
-            SET `read`=".($is_read ? 1 : 0).",
-                `date_read`=NOW()
+            SET `read`=1, `date_read`=NOW()
             WHERE `notification_id`=".$db->real_escape_string((int)$notification_id);
         if ($db->query($query)) {
             return true;
         }
         return false;
+    }
+    
+    public static function get_by_id($notification_id, $user_id = null) {
+        global $db, $CURRENT_USER;
+        
+        if ($user_id === null && $CURRENT_USER) {
+            $user_id = $CURRENT_USER->id;
+        }
+        
+        $query = "
+            SELECT `notification_id`, `message_obj_id`, `post_obj_id`,
+                `user_obj_id`, `text`, `read`, `date_read`, `date_created`
+            FROM `notifications`
+            WHERE `notification_id`=".$db->real_escape_string((int)$notification_id)."
+            AND `user_id`=".$db->real_escape_string((int)$user_id);
+        $result = $db->query($query);
+        
+        if ($result && $result->num_rows) {
+            $row = $result->fetch_assoc();
+            return Notification::create($row);
+        }
+        return null;
     }
     
     public static function get_latest($page = 1, $limit = 15) {
@@ -48,23 +88,8 @@ class Notification {
         $results = $db->query($query);
         if ($results) {
             $notifications = array();
-            while ($row = $results) {
-                $object = null;
-                $type = "object";
-                if ($row['message_obj_id']) {
-                    $object = Message::get_by_id((int)$row['message_obj_id']);
-                    $type = "message";
-                } else if ($row['post_obj_id']) {
-                    $object = Post::get_by_id((int)$row['post_obj_id']);
-                    $type = "post";
-                } else if ($row['user_obj_id']) {
-                    $object = User::get_by_id((int)$row['user_obj_id']);
-                    $type = "user";
-                }
-                $notifications[] = new Notification(
-                    $row['notification_id'], $row['text'], $row['read'],
-                    $row['date_read'], $row['date_created'], $object, $type
-                );
+            while ($row = $results->fetch_assoc()) {
+                $notifications[] = Notification::create($row);
             }
             return $notifications;
         }
