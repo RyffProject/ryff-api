56b07d0d82756ac6b1b124c3e01815e88ad0531f
Robert Fotino <robert.fotino@gmail.com>
2014-09-01 14:55:57 -0700

    Refs #55, creates an uninstall script for the database.
    ryff.sql has been moved to resources/db/install.sql, and is accompanied by resources/db/uninstall.sql. The create database and use  syntax are no longer in the install script, so that it can be used for either the main or test database.
    
    connect-db.php no longer includes config.php, that is done in global.php. connect-db will connect to the test database instead if TEST_MODE is defined and truthy.
    
    A lot of documentation has been added to SQL scripts, the config file, the
    global file, and the connect database file. The formatting on the foreign key constraints has also been changed in install.sql.

--- resources/connect-db.php
+++ resources/connect-db.php
@@ -1,9 +1,32 @@
 <?php
-
-require_once(__DIR__."/config.php");
+/**
+ * Connect Database
+ * ================
+ * 
+ * If TEST_MODE is defined and set to a truthy value, this script will attempt
+ * to connect to the test database, otherwise it will attempt to connect to the
+ * main database. The credentials for these databases are set in config.php.
+ * 
+ * If the database is connected successfully, a global variable $dbh will be
+ * created as a PDO object for accessing the database. If there is an error,
+ * the script will output an error and exit.
+ * 
+ * Ryff API <http://www.github.com/rfotino/ryff-api>
+ * Released under the Apache License 2.0.
+ */
 
 try {
-    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+    if (defined("TEST_MODE") && TEST_MODE) {
+        $dbh = new PDO(
+            "mysql:host=".TEST_DB_HOST.";dbname=".TEST_DB_NAME,
+            TEST_DB_USER, TEST_DB_PASS
+        );
+    } else {
+        $dbh = new PDO(
+            "mysql:host=".DB_HOST.";dbname=".DB_NAME,
+            DB_USER, DB_PASS
+        );
+    }
 } catch (Exception $ex) {
     header("Content-Type: application/json");
     echo json_encode(array("error" => "Unable to connect to the database."));
--- resources/db/install.sql
+++ resources/db/install.sql
@@ -1,19 +1,22 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+--
+-- Install Database
+-- ================
+--
+-- Creates tables and foreign keys for the database.
+--
+-- Ryff API <http://www.github.com/rfotino/ryff-api>
+-- Released under the Apache License 2.0.
+--
+
 
 --
--- Database: `ryff`
+-- Table Structures
+-- ----------------
 --
-
-CREATE DATABASE IF NOT EXISTS `ryff`;
-USE `ryff`;
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `apns_tokens`
 --
-
 CREATE TABLE IF NOT EXISTS `apns_tokens` (
   `apns_token_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
@@ -24,12 +27,9 @@ CREATE TABLE IF NOT EXISTS `apns_tokens` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `auth_tokens`
 --
-
 CREATE TABLE IF NOT EXISTS `auth_tokens` (
   `token_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
@@ -40,12 +40,9 @@ CREATE TABLE IF NOT EXISTS `auth_tokens` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `conversation_members`
 --
-
 CREATE TABLE IF NOT EXISTS `conversation_members` (
   `conversation_member_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `conversation_id` int(10) unsigned NOT NULL,
@@ -57,12 +54,9 @@ CREATE TABLE IF NOT EXISTS `conversation_members` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `conversations`
 --
-
 CREATE TABLE IF NOT EXISTS `conversations` (
   `conversation_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -70,12 +64,9 @@ CREATE TABLE IF NOT EXISTS `conversations` (
   PRIMARY KEY (`conversation_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `follows`
 --
-
 CREATE TABLE IF NOT EXISTS `follows` (
   `follow_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `to_id` int(10) unsigned NOT NULL,
@@ -86,12 +77,9 @@ CREATE TABLE IF NOT EXISTS `follows` (
   KEY `from_id` (`from_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `locations`
 --
-
 CREATE TABLE IF NOT EXISTS `locations` (
   `location_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
@@ -101,12 +89,9 @@ CREATE TABLE IF NOT EXISTS `locations` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `messages`
 --
-
 CREATE TABLE IF NOT EXISTS `messages` (
   `message_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `conversation_id` int(10) unsigned NOT NULL,
@@ -120,12 +105,9 @@ CREATE TABLE IF NOT EXISTS `messages` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `notification_objects`
 --
-
 CREATE TABLE IF NOT EXISTS `notification_objects` (
   `notification_object_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `notification_id` int(10) unsigned NOT NULL,
@@ -140,12 +122,9 @@ CREATE TABLE IF NOT EXISTS `notification_objects` (
   KEY `user_obj_id` (`user_obj_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `notifications`
 --
-
 CREATE TABLE IF NOT EXISTS `notifications` (
   `notification_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
@@ -162,12 +141,9 @@ CREATE TABLE IF NOT EXISTS `notifications` (
   KEY `user_obj_id` (`user_obj_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `post_families`
 --
-
 CREATE TABLE IF NOT EXISTS `post_families` (
   `post_family_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `parent_id` int(10) unsigned NOT NULL,
@@ -178,12 +154,9 @@ CREATE TABLE IF NOT EXISTS `post_families` (
   KEY `child_id` (`child_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `post_tags`
 --
-
 CREATE TABLE IF NOT EXISTS `post_tags` (
   `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `post_id` int(10) unsigned NOT NULL,
@@ -193,12 +166,9 @@ CREATE TABLE IF NOT EXISTS `post_tags` (
   KEY `post_id` (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `posts`
 --
-
 CREATE TABLE IF NOT EXISTS `posts` (
   `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
@@ -208,12 +178,9 @@ CREATE TABLE IF NOT EXISTS `posts` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `riffs`
 --
-
 CREATE TABLE IF NOT EXISTS `riffs` (
   `riff_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `post_id` int(10) unsigned NOT NULL,
@@ -224,12 +191,9 @@ CREATE TABLE IF NOT EXISTS `riffs` (
   KEY `post_id` (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `stars`
 --
-
 CREATE TABLE IF NOT EXISTS `stars` (
   `star_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `post_id` int(10) unsigned NOT NULL,
@@ -240,12 +204,9 @@ CREATE TABLE IF NOT EXISTS `stars` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `upvotes`
 --
-
 CREATE TABLE IF NOT EXISTS `upvotes` (
   `upvote_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `post_id` int(10) unsigned NOT NULL,
@@ -256,12 +217,9 @@ CREATE TABLE IF NOT EXISTS `upvotes` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `user_tags`
 --
-
 CREATE TABLE IF NOT EXISTS `user_tags` (
   `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
@@ -271,12 +229,9 @@ CREATE TABLE IF NOT EXISTS `user_tags` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
 --
 -- Table structure for table `users`
 --
-
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -289,106 +244,183 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+
 --
--- Constraints for dumped tables
+-- Constraints
+-- -----------
 --
 
 --
 -- Constraints for table `apns_tokens`
 --
 ALTER TABLE `apns_tokens`
-  ADD CONSTRAINT `apns_tokens_user_id_constr` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `apns_tokens_user_id_constr`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `auth_tokens`
 --
 ALTER TABLE `auth_tokens`
-  ADD CONSTRAINT `auth_tokens_user_id_constr` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `auth_tokens_user_id_constr`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `conversation_members`
 --
 ALTER TABLE `conversation_members`
-  ADD CONSTRAINT `conversation_members_conversation_id_constr` FOREIGN KEY (`conversation_id`) REFERENCES `conversations` (`conversation_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `conversation_members_user_id_constr` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `conversation_members_conversation_id_constr`
+    FOREIGN KEY (`conversation_id`)
+    REFERENCES `conversations` (`conversation_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `conversation_members_user_id_constr`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `follows`
 --
 ALTER TABLE `follows`
-  ADD CONSTRAINT `follows_from_id_constr` FOREIGN KEY (`from_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `follows_to_id_constr` FOREIGN KEY (`to_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `follows_from_id_constr`
+    FOREIGN KEY (`from_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `follows_to_id_constr`
+    FOREIGN KEY (`to_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `locations`
 --
 ALTER TABLE `locations`
-  ADD CONSTRAINT `locations_user_id_constr` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `locations_user_id_constr`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `messages`
 --
 ALTER TABLE `messages`
-  ADD CONSTRAINT `messages_conversation_id_constr` FOREIGN KEY (`conversation_id`) REFERENCES `conversations` (`conversation_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `messages_user_id_constr` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `messages_conversation_id_constr`
+    FOREIGN KEY (`conversation_id`)
+    REFERENCES `conversations` (`conversation_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `messages_user_id_constr`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `notification_objects`
 --
 ALTER TABLE `notification_objects`
-  ADD CONSTRAINT `notification_id_constr` FOREIGN KEY (`notification_id`) REFERENCES `notifications` (`notification_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `notification_objects_post_obj_id_constr` FOREIGN KEY (`post_obj_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `notification_objects_user_obj_id_constr` FOREIGN KEY (`user_obj_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `notification_id_constr`
+    FOREIGN KEY (`notification_id`)
+    REFERENCES `notifications` (`notification_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notification_objects_post_obj_id_constr`
+    FOREIGN KEY (`post_obj_id`)
+    REFERENCES `posts` (`post_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notification_objects_user_obj_id_constr`
+    FOREIGN KEY (`user_obj_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `notifications`
 --
 ALTER TABLE `notifications`
-  ADD CONSTRAINT `notifications_user_id_constr` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `notifications_post_obj_id_constr` FOREIGN KEY (`post_obj_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `notifications_user_obj_id_constr` FOREIGN KEY (`user_obj_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `notifications_user_id_constr`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notifications_post_obj_id_constr`
+    FOREIGN KEY (`post_obj_id`)
+    REFERENCES `posts` (`post_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notifications_user_obj_id_constr`
+    FOREIGN KEY (`user_obj_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `post_families`
 --
 ALTER TABLE `post_families`
-  ADD CONSTRAINT `post_families_parent_id_constr` FOREIGN KEY (`parent_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `post_families_child_id_constr` FOREIGN KEY (`child_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `post_families_parent_id_constr`
+    FOREIGN KEY (`parent_id`)
+    REFERENCES `posts` (`post_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `post_families_child_id_constr`
+    FOREIGN KEY (`child_id`)
+    REFERENCES `posts` (`post_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `post_tags`
 --
 ALTER TABLE `post_tags`
-  ADD CONSTRAINT `tags_post_id_constr` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `tags_post_id_constr`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `posts` (`post_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `posts`
 --
 ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_user_id_constr` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `posts_user_id_constr`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `riffs`
 --
 ALTER TABLE `riffs`
-  ADD CONSTRAINT `riffs_post_id_constr` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `riffs_post_id_constr`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `posts` (`post_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `stars`
 --
 ALTER TABLE `stars`
-  ADD CONSTRAINT `stars_post_id_constr` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `stars_user_id_constr` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `stars_post_id_constr`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `posts` (`post_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `stars_user_id_constr`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `upvotes`
 --
 ALTER TABLE `upvotes`
-  ADD CONSTRAINT `upvotes_post_id_constr` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `upvotes_user_id_constr` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `upvotes_post_id_constr`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `posts` (`post_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `upvotes_user_id_constr`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user_tags`
 --
 ALTER TABLE `user_tags`
-  ADD CONSTRAINT `tags_user_id_constr` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `tags_user_id_constr`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
